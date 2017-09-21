2b637076e6ae12236d4d24e93f0f1b3f00d2bdc1
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-21 19:59:26 -0300

    Evoluindo no controller Sessions para login e logout dos usuários

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
+    person = Person.auth(params[:email], params[:senha])
+    if !person
+      redirect_to new_session_url
+      return
+    end
+    session[:id]    = person.id
+    session[:name]  = person.name
+    session[:admin] = person.admin
+    flash[:notice]  = "Olá, #{person.name}!"
+    redirect_to people_path
   end
 
   def destroy
+    session[:id] = nil
+    redirect_to new_session_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   resources :people
   resources :sessions, only: %i(new create destroy)
-  
+
 end
