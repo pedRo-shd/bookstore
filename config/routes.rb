09c09d82a9f088d4303401ce8903573f9dd0c748
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-21 19:54:51 -0300

    Criando controller Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :people
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: %i(new create destroy)
+  
 end
