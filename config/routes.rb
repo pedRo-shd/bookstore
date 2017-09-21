f19f636d3dc0ea7ba2167502596a6e47ac25f409
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-21 20:10:37 -0300

    Customizando URLs de session p/ /autenticar e /sair

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :people
   resources :sessions, only: %i(new create destroy)
 
+  get "autenticar"  => "sessions#new"
+  post "autenticar" => "sessions#create"
+  delete "sair"     => "sessions#destroy"
 end
