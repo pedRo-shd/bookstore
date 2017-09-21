7681c01dfd7ae68dff03a6d381ce64b3620b82ab
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-20 22:52:56 -0300

    Validações para campos dos usuários (Person)

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,21 @@
 class Person < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :born_at, presence: true
+  validate :age_limit
+
+  validates :email, allow_blank: true, allow_nil: true, uniqueness: true,
+    format: {
+      with: /\A[a-zA-Z0-9_.-]+@([a-zA-Z0-9_ -]+\.)+[a-zA-Z]{2,4}\z/
+    }
+
+
+  private
+
+  def age_limit
+    if self.born_at.nil? || Date.current.year - self.born_at.year < 16
+      errors.add(:born_at, 'tem que ser maior que 16 anos')
+      throw(:abort)
+    end
+  end
 end
