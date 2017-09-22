acfaed9650f9b412405047d62b7b93968690722f
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-21 21:56:13 -0300

    Alterado view e controller para admin de sessão marcar um usuário como admin

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,50 +2,39 @@ class PeopleController < LoggedController
   respond_to :html
   before_action :set_person, only: [:show, :edit, :update, :destroy]
 
-  # GET /people
-  # GET /people.json
   def index
     @people = Person.all
   end
 
-  # GET /people/1
-  # GET /people/1.json
   def show
   end
 
-  # GET /people/new
   def new
     @person = Person.new
   end
 
-  # GET /people/1/edit
   def edit
   end
 
-  # POST /people
-  # POST /people.json
   def create
     @person = Person.new(person_params)
-    flash[:notice] = 'Pessoa salva' if @person.save
+    @person.admin = params[:admin] if session[:admin]
+    flash[:notice] = "Pessoa salva" if @person.save
     respond_with @person
   end
 
-  # PATCH/PUT /people/1
-  # PATCH/PUT /people/1.json
   def update
-    flash[:notice] = 'Pessoa atualizada' if @person.update(person_params)
+    @person.admin = params[:admin] if session[:admin]
+    flash[:notice] = "Pessoa atualizada" if @person.update(person_params)
     respond_with @person
   end
 
-  # DELETE /people/1
-  # DELETE /people/1.json
   def destroy
     flash[:notice] = 'Pessoa apagada' if @person.destroy
     respond_with @person
   end
 
   private
-
 
   def set_person
     @person = Person.find(params[:id]) rescue nil
