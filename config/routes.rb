a538a42fc3a3ac58177fe4656223425d8d386385
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-21 22:41:22 -0300

    Criado scaffold para categorias dos livros

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :categories
   resources :books
   root 'pub#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922012752) do
+ActiveRecord::Schema.define(version: 20170922013942) do
 
   create_table "books", force: :cascade do |t|
     t.string "title", limit: 100, null: false
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20170922012752) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["person_id"], name: "index_books_on_person_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "people", force: :cascade do |t|
