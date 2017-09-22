d17f45f4d86215e197262893ee75d6955d040c38
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-21 22:29:56 -0300

    Ajustes na migrate para respeitar regra de negócio

--- db/migrate/20170922012752_create_books.rb
+++ db/migrate/20170922012752_create_books.rb
@@ -1,12 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
-      t.string :title
-      t.date :published_at
-      t.text :text
-      t.decimal :value
+      t.string :title, limit: 100, null: false
+      t.date :published_at, null: false
+      t.text :text, null: false
+      t.decimal :value, precision: 10, scale: 2, null: false
       t.references :person, foreign_key: true
-
       t.timestamps
     end
   end
