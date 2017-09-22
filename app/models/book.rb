c7095efa777ddb8254c37992af7e8c86ad705561
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-21 22:49:51 -0300

    Associação muitos para muitos, onde um livro pode ter varias categorias e vice versa

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,11 @@
 class Book < ApplicationRecord
-  belongs_to :person
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :published_at, presence: true
   validates :text, presence: true
   validates :value, presence: true, numericality: { less_than_or_equal_to: 99999999.99 }
   validates :person, presence: true
+
+  belongs_to :person
+  has_and_belongs_to_many :categories
 end
