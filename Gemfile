062acfe70da0d87be86438e1e82bba1a751e4b74
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-23 15:21:04 -0300

    Criado sessões para utilizar carrinho de compras

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'jquery-ui-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'activerecord-session_store'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923180816) do
+ActiveRecord::Schema.define(version: 20170923181832) do
 
   create_table "books", force: :cascade do |t|
     t.string "title", limit: 100, null: false
@@ -73,6 +73,15 @@ ActiveRecord::Schema.define(version: 20170923180816) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_people_on_email", unique: true
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
 end
