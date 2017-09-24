4de52b8932b61bee3c45cd8724ce21ff877c8e6f
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-24 19:11:29 -0300

    Exclusão de arquivos que não serão utilizados como carrinho de compras e itens de pedido

--- app/models/order_item.rb
+++
@@ -1,9 +0,0 @@
-class OrderItem < ApplicationRecord
-
-  validates :order, presence: true
-  validates :book, presence: true
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
-
-  belongs_to :order
-  belongs_to :book
-end
--- db/migrate/20170924021345_create_orders.rb
+++ db/migrate/20170924021345_create_orders.rb
@@ -2,6 +2,10 @@ class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
       t.references :person, foreign_key: true, index: true, null: false
+      t.references :book, foreign_key: true, index: true, null: false
+      t.integer :quantity, null: false
+      t.decimal :value, precision: 15, scale: 2, null: false
+
       t.timestamps
     end
   end
