303f5eddb540269e943310e34d0759191ad7782e
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-24 18:51:57 -0300

    Helper para login e logout na view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
-end
+
+  def auth_link
+      if session[:id].present?
+        link_to 'Logout', sair_path(session[:id]), method: 'delete'
+      else
+        link_to 'Login', autenticar_path
+      end
+    end
+
+  end
