25e2430c05f57069fc1b9bd998fee98a4c248617
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-24 18:49:56 -0300

    Ajustes para o pedido de compra, enviando o livro selecionado para gerar o pedido

--- app/controllers/pub_controller.rb
+++ app/controllers/pub_controller.rb
@@ -1,62 +1,44 @@
 class PubController < ApplicationController
+  before_action :set_book, only: [:book, :buy]
 
   def index
     @books = Book.all
   end
 
   def book
-    @book = Book.find(params[:id])
   end
 
   def author
     @author = Person.find(params[:id])
   end
 
-  def search
-    render json: Book.all.pluck(:title)
-  end
-
-  def find_cart
-    session[:cart] ||= Cart.new
-  end
-
   def buy
-    find_cart << Book.find(params[:id])
-    redirect_to action: :cart
-  end
-
-  def cart
-    @cart = find_cart
-  end
-
-  def remove
-    @book = Book.find(params[:id])
-    @cart = find_cart
-    @cart - @book
-    redirect_to cart_path
-  end
-
-  def change
-    @book = Book.find(params[:id])
-    @cart = find_cart
-    @cart.change(@book, params[:qty].to_i)
-  end
-
-  def close_order
   raise NotAuthenticated, 'Faça o login primeiro' if session[:id].blank?
-  @cart = find_cart
-  @order = Order.create_by_cart(session[:id], @cart.items)
+  @order = Order.create_by_cart(session[:id], @book)
 
   if @order.blank?
     flash[:notice] = 'Não foi possível criar o seu pedido!'
     redirect_to root_path
     return
   end
-    @cart.clear
     redirect_to order_path(@order)
   end
 
   def order
     @order = Order.find(params[:id])
+  end
+
+  def find_cart
+    session[:cart] ||= Cart.new
+  end
+
+  def search
+    render json: Book.all.pluck(:title)
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
