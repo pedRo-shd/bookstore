8ce48cc8be2fb8380379b934a46f945215a61a03
pedRo-shd <pedrocustodio@outlook.com.br>
2017-09-24 19:03:40 -0300

    Ajustes nas validações de pessoa + ajuste no método que verifica idade

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,24 +1,24 @@
 class Person < ApplicationRecord
-
-  validates :password, presence: { on: :create }, length: { minimum: 8, allow_blank: true }
+  include ImageSaver
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :born_at, presence: true
-  validate :age_limit
   validates :email, allow_blank: true, allow_nil: true,
                     uniqueness: true,
-                    format: {with: /\A[a-zA-Z0-9_.-]+@([a-zA-Z0-9_ -]+\.)+[a-zA-Z]{2,4}\z/}
+                    format: { with: /\A[a-zA-Z0-9_.-]+@([a-zA-Z0-9_ -]+\.)+[a-zA-Z]{2,4}\z/ }
+  validates :born_at, presence: true
+  validate  :age_limit
+  validates :password, presence: { on: :create }, length: { minimum: 8, allow_blank: true }
 
-  scope :admins, -> { where(admin: true) }
-  scope :recent, -> { where(["created_at >= ?", 7.days.ago]) }
-  default_scope -> { order(:name) }
+  scope :admins,    -> { where(admin: true) }
+  scope :recent,    ->(days = 7) { where('created_at >= ?', days.days.ago) }
+  default_scope     -> { order(:name) }
 
   has_secure_password
 
-  has_many :orders
   has_many :books, dependent: :restrict_with_exception
-  has_one :image, dependent: :destroy
+  has_many :categories, -> { distinct }, through: :books
   has_one :image, dependent: :destroy, as: :imageable
+  has_many :orders
 
   def self.auth(email, senha)
     person = Person.where(email: email).first
@@ -31,10 +31,9 @@ class Person < ApplicationRecord
 
   private
 
-
   def age_limit
-    if self.born_at.nil? || Date.current.year - self.born_at.year < 16
-      errors.add(:born_at, 'tem que ser maior que 16 anos')
+    if born_at.nil? || Date.current.year - born_at.year < 16
+      errors.add(:born_at, "Deve ser maior de 16 anos")
       throw(:abort)
     end
   end
